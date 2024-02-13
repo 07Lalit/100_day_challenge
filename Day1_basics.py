b3063349ac161a4766636a5cd585cb4cc418e93e
lalit <gourlalit11@gamil.com>
2024-02-14 02:36:11 +0530

    dsa revision from basic to advance day1

---
+++ Day1_basics.py
@@ -0,0 +1,80 @@
+"""
+                       # Introduction to DSA
+                       ------------------------
+
+# What is data ?
+=> Data are raw fact's and figures (unprocessed)
+
+# What is Structure?
+=> It is a way of organizing .
+
+# Algorithms ?
+=> It is a step-by-step procedure to solve any problem .
+
+# data structure ?
+=> Something which store data .
+   like list, int , tuple, string , boolean , complex .
+
+=> Data Structure is a way of storing and organizing data
+  in computer memory in such a way we can use them , able to
+  apply operation on them in more convenient(easy not complex) and
+  efficient (less resource consumed) them .
+
+Type of data structure:
+1) Primitive data structure : Inbuilt in programming language .
+                        int , string ,bool , complex,float ,double
+2) Non-primitive data structure : they are defined by programmers classify into
+                        two types :
+        2.1) Linear
+       -> Array
+       -> Stack
+       -> Queue
+       -> Linkedlist
+
+       2.2) Non-linear
+       -> Tree
+       -> Graph
+
+# Linear Data Structure : The elements/data stored in this data structure are
+                      connected in a sequence manner by mean of logically or
+                      in sequence memory location .
+
+        array : sequential and continuous
+        linkedlist : sequential but not continuous
+
+        Every continuous ds is also sequential data structure.
+
+# Non-linear DS : data is not arranged in sequential manner .
+                they are arranged in hierarchical manner .
+                ex : tree and graph
+
+1) intro to ds
+2) recursion
+3) Time and space complexity
+4) list
+5) string
+6) searching
+7) sorting  6 type
+8) linkedlist
+9) stack
+10) Queue-> singly ,circular, doubly,
+11) heap
+12) Tree -> binary tree, binary search tree, AVl , red -black tree , spanning tree ,segemnt tree .
+13) Graph -> bfs , dfs , theory , connected component , adjacency matrix , list rep.
+14) Huffman coding
+15) divide and conquer
+16) Greedy
+17) DP
+18)game theory
+19) prisms
+19) kruskal
+20) dijkastra algorithm
+21) Bellman-ford
+22) mathematics , probability, statics
+
+
+
+
+"""
+
+
---
+++ Recursion.py
@@ -0,0 +1,30 @@
+"""
+                 Recursion
+                 ---------
+
+=> Programming technique in which a function call inside it's body
+   repeatedly with a specific condition reached
+-> this used to solve bigger complex problem
+-> time-consuming .
+
+ loops or recursion -> fibbonaci , factorial
+ only recursion ->  iterative way is not applicable
+ -> tower of Hanoi
+ -> josephous problem
+ -> permutation
+
+ application -> Dynamic programming , divide and conquer , TOH , DFS traversal
+               Tree .
+
+ex :
+
+    def fun():
+
+        fun()   #recursive function call it's inside it's body
+
+
+
+
+
+
+"""
